267e46ea0ecf38e4f62e2ef2853657d30fa21602
Gabriella <gabriella.thorne1@gmail.com>
2023-06-14 09:35:37 -0400

    Updated Docker files with fully working web app

--- app.py
+++ app.py
@@ -15,19 +15,27 @@ def index():
 
 @app.route("/home")
 def home(): 
-    return render_template('home.html')
+    return render_template("home.html")
 
 @app.route("/info")
 def info():
-    return render_template('info.html')
+    return render_template("info.html")
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
-    
+    return render_template("login.html")
+
+@app.route("/Oak")
+def Oak():
+    return render_template("Oak.html")
+
+@app.route("/submit")
+def submit():
+    return render_template("submit") 
+
 @app.route("/project3")
 def project3():
-    return render_template('project3.html')    
+    return render_template("project3.html")    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
